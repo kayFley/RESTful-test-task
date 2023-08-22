e424659336e305c43c210abe10ea30936f5bf2d4
-kF <134998691+kayFley@users.noreply.github.com>
2023-08-23 00:33:31 +0700

    Update req.py

--- req.py
+++ req.py
@@ -1,7 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/payments'
-
+# url = 'http://localhost:5000/payments'
 # data = {
 #     "date": "2023.04.01",
 #     "amount": 100.50
@@ -10,10 +9,17 @@ url = 'http://localhost:5000/payments'
 # response = requests.post(url, json=data, headers=headers)
 # print(response.json())
 
+# url = 'http://localhost:5000/payments'
 # params = {'date1': '2023.01.01'}
 # response = requests.get(url, params=params)
 # print(response.json())
 
-params = {'date1': '2023.01.01', 'date2': '2023.12.31'}
-response = requests.get(url, params=params)
+# url = 'http://localhost:5000/payments'
+# params = {'date1': '2023.01.01', 'date2': '2023.12.31'}
+# response = requests.get(url, params=params)
+# print(response.json())
+
+url = 'http://localhost:5000/payments/1'
+params = {'date': '2023.06.03', 'amount': 150}
+response = requests.put(url, json=params)
 print(response.json())
